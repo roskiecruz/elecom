8ba88d94f84e564b3b3110afcb76c525fdce2810
rorypond <ian.dejesus.fontilea@gmail.com>
2013-07-08 01:45:06 +0800

    trying out passwrds
    its seems to be right?! hehehe

--- passwords.php
+++ passwords.php
@@ -1,12 +1,45 @@
-<?php 
-$USERS["username1"] = "password1"; 
-$USERS["username2"] = "password2"; 
-$USERS["username3"] = "password3"; 
-  
-function check_logged(){ 
-     global $_SESSION, $USERS; 
-     if (!array_key_exists($_SESSION["logged"],$USERS)) { 
-          header("Location: login.php"); 
-     }; 
-}; 
+<?php
+
+/*
+$host="localhost"; // Host name 
+$username=""; // Mysql username 
+$password=""; // Mysql password 
+$db_name="test"; // Database name 
+$tbl_name="members"; // Table name 
+
+// Connect to server and select databse.
+mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+*/
+
+// username and password sent from form 
+$myusername=$_POST['myusername']; 
+$mypassword=$_POST['mypassword']; 
+$defaultUser='CD';
+$defaultpass='CD';
+/*
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$result=mysql_query($sql);
+
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+*/
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if ($USERS[$_POST["myusername"]]==$defaultUser && $USERS[$_POST["mypassword"]]==$defaultpass){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_register("myusername");
+session_register("mypassword"); 
+header("location:studentHome.html");
+}
+else {
+echo "Wrong Username or Password";
+}
 ?>
