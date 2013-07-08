53baeb4b7cf449b193ef9fb754082dc7bac1f4ec
roskiecruz <roskiecruz@gmail.com>
2013-07-08 22:42:01 +0800

    working data connection
    working data connection from studentVote.html to success.php
    thru Javascript value manipulation

--- success.php
+++ success.php
@@ -1,3 +1,3 @@
-<?php echo $_POST['CampPres']; ?>!<br/>
-<?php echo $_POST['LegAsembl']; ?><br/>
+<?php echo $_POST['CampPresVar']; ?><br/>
+<?php echo $_POST['LegAsemblVar']; ?><br/>
 <?php echo $_POST['ColRepVar']; ?><br/>
